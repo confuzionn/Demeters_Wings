0aab6e98d17926a57f026f18340c832064c906b8
Nicholas Rusali <149333463+Nick-M-R@users.noreply.github.com>
2025-01-22 17:23:28 -0800

    Weight estimation steps

--- Assignment 2/Weight Estimation Code.py
+++ Assignment 2/Weight Estimation Code.py
@@ -1,3 +1,10 @@
 # Weight Estimation Code
 # Last Updated: 1/22/2025
 
+
+#-------------------------------------------------------------------------------------------------------------------------#
+# Weight Estimation Steps
+    # Step 1: Establish Requirements
+    # Step 2: Obtain Historical Market Data
+    # Step 3: List Assumptions
+    # Step 4: Compute Payload Weight (Human + Cargo)
