af09f3a7dc436a2aeb4101479ee03932e8a0355e
Nicholas Rusali <nmrusali@ucdavis.edu>
2025-01-28 14:54:12 -0800

    step 1 done

--- Assignment 2/Weight Estimation Code.py
+++ Assignment 2/Weight Estimation Code.py
@@ -8,3 +8,18 @@
     # Step 2: Obtain Historical Market Data
     # Step 3: List Assumptions
     # Step 4: Compute Payload Weight (Human + Cargo)
+
+
+# Step 1: Establish Requirements
+
+# Design Mission Profile
+des_rad = 25#nmi (design radius)
+des_area = 400#acres (20x20)
+reserves = 30#min (additional fuel)
+cargo = 2000#lb (liquid or solid materials)
+passenger = 160#lb (single person avg)
+payload = cargo + passenger
+
+MTOW = 19,000#lb (max gross weight/max takeoff weight)
+
+#Step 2: Obtain Historical/Market Data
