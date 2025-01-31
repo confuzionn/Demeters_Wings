e828ebac423a66d56907c059680584a6a97b2f98
Jphervey <jphervey@ucdavis.edu>
2025-01-31 11:23:44 -0800

    Update 2025_01_31_A2_Group4_CostAnalysis.py

--- Assignment 2/2025_01_31_A2_Group4_CostAnalysis.py
+++ Assignment 2/2025_01_31_A2_Group4_CostAnalysis.py
@@ -20,17 +20,17 @@ SHP_TO = 3000
 C_aircraft, C_engine, C_airframe = get_rough_aircraft_cost(MTOW, SHP_TO)
 print('Rough Estimated Cost: ${}'.format(C_aircraft))
 
-# ------------------------- RAND DAPCA IV Model for RDT&E and Production Costs ------------------------- #
+# ------------------------- RAND DAPCA IV Model for RDT&E and Production (10% Flyaway) Costs ------------------------- #
 
 def RAND_DAPCAP_IV(W_e, V, Q):
-    HE = 4.86 * (W_e)**0.777 * V**0.894 * Q**0.163      # Engineering hours
-    HT = 5.99 * (W_e)**0.777 * V**0.696 * Q**0.263      # Tooling hours
-    HM = 7.37 * (W_e)**0.82 * V**0.0484 * Q**0.641      # Manufacturing hours
+    HE = 4.86 * (W_e**0.777) * (V**0.894) * (Q**0.163)      # Engineering hours
+    HT = 5.99 * (W_e**0.777) * (V**0.696) * (Q**0.263)      # Tooling hours
+    HM = 7.37 * (W_e**0.82) * (V**0.0484) * (Q**0.641)      # Manufacturing hours
     HQ = 0.133 * HM                                     # Quality control hours
-    CD = 91.3 * (W_e)**0.630 * V**1.3                   # Development support cost
+    CD = 91.3 * (W_e**0.630) * (V**1.3)                   # Development support cost
     FTA = 2                                             # Number of flight test aircraft
-    CF = 2498 * (W_e)**0.325 * V**0.822 * (FTA)**1.21   # Flight test cost
-    CM= 22.1 * (W_e)**0.921 * V**0.621 * Q**0.799       # Manufacturing materials cost
+    CF = 2498 * (W_e**0.325) * (V**0.822) * (FTA**1.21)   # Flight test cost
+    CM = 22.1 * (W_e**0.921) * (V**0.621) * (Q**0.799)      # Manufacturing materials cost
     
     # Hourly rate linear fit equations
     y = 2024              # Production year
@@ -39,12 +39,13 @@ def RAND_DAPCAP_IV(W_e, V, Q):
     RQ = 2.60 * y - 5112  # Quaility rate
     RM = 2.316 * y - 4552 # Manufacturing rate
 
-    RDTE_flyaway = (HE * RE) + (HT * RT) + (HM * RM) + (HQ * RQ) + CD + CF + CM # + (C_eng * N_eng) + C_avionics
-    return RDTE_flyaway
+    RDTE = (HE * RE) + (HT * RT) + (HM * RM) + (HQ * RQ) + CD + CF + CM
+    #flyaway = (C_eng * N_eng) + C_avionics
+    return RDTE
+RDTE_flyaway = RAND_DAPCAP_IV(10000, 250 , 10)
+print(RDTE_flyaway)
 
-RDTE_flyaway = RAND_DAPCAP_IV(10000, )
-
-# ------------------------- Operation and Maintenance Costs ------------------------- #
+# ------------------------- Operation and Maintenance Costs (DOC) ------------------------- #
 
 def operation_maintenance(t_b, CEF, W_A, R, RL):
     C_crew = (440 + 0.532 (MTOW/1000) * (CEF) * (t_b))
